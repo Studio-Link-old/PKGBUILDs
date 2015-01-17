7b1f51d07aa983c6ec1c5c43e7eca7609290dde1
Sebastian Reimers <info@miete-admin.de>
2015-01-17 16:46:45 +0100

    qemu multi core build and fix pacman download warnings/errors

--- makepkg.sh
+++ makepkg.sh
@@ -5,7 +5,7 @@ scp="scp -P2222 -r root@127.0.0.1:"
 pacman="pacman --noconfirm --force --needed"
 version="15.1.0-beta"
 export QEMU_AUDIO_DRV=none
-$qemu -daemonize -M vexpress-a9 -kernel zImage \
+$qemu -daemonize -M vexpress-a9 -smp 2 -kernel zImage \
 	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
 	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
 sleep 20
@@ -32,16 +32,21 @@ $ssh "chown -R nobody /tmp/PKGBUILDs"
 $ssh "echo -e 'root ALL=(ALL) ALL\nnobody ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers"
 
 echo "### Build ###"
+$ssh "echo 'MAKEFLAGS=\"-j2\"' >> /etc/makepkg.conf"
 makepkg="sudo -u nobody makepkg --force --install --noconfirm --syncdeps"
 $ssh "cd /tmp/PKGBUILDs/opus; $makepkg"
 $ssh "cd /tmp/PKGBUILDs/jack2; $makepkg"
 $ssh "cd /tmp/PKGBUILDs/libre; $makepkg"
 $ssh "cd /tmp/PKGBUILDs/librem; $makepkg"
 $ssh "cd /tmp/PKGBUILDs/baresip; $makepkg"
+#$ssh "cd /tmp/PKGBUILDs/jack_capture; $makepkg"
+#$ssh "cd /tmp/PKGBUILDs/aj-snapshot; $makepkg"
+#$ssh "cd /tmp/PKGBUILDs/jack_gaudio; $makepkg"
+#$ssh "cd /tmp/PKGBUILDs/darkice; $makepkg"
 
 echo "### Download all packages ###"
 $ssh "pacman -Qq > /tmp/packages"
-$ssh "bash -c \"pacman --noconfirm --force -Sw \$(cat /tmp/packages|tr '\n' ' ')\""
+$ssh "bash -c \"pacman --noconfirm --force -Sw \$(cat /tmp/packages|tr '\n' ' ')\"" || true
 $ssh "repo-add /root/studio-link.db.tar.gz /var/cache/pacman/pkg/*.pkg.tar.xz"
 
 mkdir -p /var/www/$version
