ead0a7cc9c95e3809f83a970249738b8b2593c1b
Sebastian Reimers <info@miete-admin.de>
2015-01-17 17:31:40 +0100

    revert smp config

--- makepkg.sh
+++ makepkg.sh
@@ -5,7 +5,7 @@ scp="scp -P2222 -r root@127.0.0.1:"
 pacman="pacman --noconfirm --force --needed"
 version="15.1.0-beta"
 export QEMU_AUDIO_DRV=none
-$qemu -daemonize -M vexpress-a9 -smp 2 -kernel zImage \
+$qemu -daemonize -M vexpress-a9 -kernel zImage \
 	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
 	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
 sleep 20
@@ -32,7 +32,6 @@ $ssh "chown -R nobody /tmp/PKGBUILDs"
 $ssh "echo -e 'root ALL=(ALL) ALL\nnobody ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers"
 
 echo "### Build ###"
-$ssh "echo 'MAKEFLAGS=\"-j2\"' >> /etc/makepkg.conf"
 makepkg="sudo -u nobody makepkg --force --install --noconfirm --syncdeps"
 $ssh "cd /tmp/PKGBUILDs/opus; $makepkg"
 $ssh "cd /tmp/PKGBUILDs/jack2; $makepkg"
